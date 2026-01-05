25e6b8cb5a30bb1aa942a89bc0d140d14f28e1d5
Fabian Heinrich <fabianheinrich@aol.com>
2026-01-05 12:50:02 +0100

    wip macos settings

--- hosts/legendre/darwin.nix
+++ hosts/legendre/darwin.nix
@@ -28,6 +28,10 @@
         autohide = true;
         orientation = "right";
         show-recents = false;
+        launchanim = false;
+        tilesize = 64;
+        minimize-to-application = true;
+        mineffect = "scale";
         persistent-apps = [
           "/Applications/Ghostty.app/"
           "/Applications/Zen.app"
@@ -45,11 +49,17 @@
         FXDefaultSearchScope = "SCcf";
         FXPreferredViewStyle = "Nlsv";
         NewWindowTarget = "Other";
-        NewWindowTargetPath = "file:///Users/fabian/";
         QuitMenuItem = true;
         ShowPathbar = true;
         ShowStatusBar = true;
         _FXShowPosixPathInTitle = true;
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowHardDrivesOnDesktop = false;
+        ShowMountedServersOnDesktop = false;
+        ShowRemovableMediaOnDesktop = true;
+        _FXSortFoldersFirst = true;
+        NewWindowTargetPath = "file://$\{HOME\}";
+        FXEnableExtensionChangeWarning = false;
       };
       menuExtraClock = {
         ShowDayOfMonth = true;
@@ -61,6 +71,69 @@
         Clicking = true;
         TrackpadRightClick = true;
         TrackpadThreeFingerDrag = true;
+      };
+      CustomUserPreferences = {
+        NSGlobalDomain = {
+          AppleCalendar = "gregorian";
+          AppleLocale = "en_US";
+          AppleFirstWeekday = {
+            gregorian = 2;
+          };
+          AppleMeasurementUnits = "Centimeters";
+          AppleICUDateFormatStrings = {
+            "1" = "MM/dd/yyyy";
+          };
+          AppleICUNumberFormatStrings = {
+            "1" = "#,##0.###";
+          };
+          ApplePressAndHoldEnabled = false;
+          InitialKeyRepeat = 15;
+          KeyRepeat = 2;
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+        };
+        "com.apple.desktopservices" = {
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.screensaver" = {
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+        "com.apple.loginwindow" = {
+          GuestEnabled = false;
+        };
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+        "com.apple.SoftwareUpdate" = {
+          AutomaticCheckEnabled = true;
+          # Check for software updates daily, not just once per week
+          ScheduleFrequency = 1;
+          # Download newly available updates in background
+          AutomaticDownload = 1;
+          # Install System data files & security updates
+          CriticalUpdateInstall = 1;
+        };
+        "com.apple.HIToolbox" = {
+          AppleEnabledInputSources = [
+            {
+              InputSourceKind = "Keyboard Layout";
+              "KeyboardLayout ID" = 0;
+              "KeyboardLayout Name" = "U.S.";
+            }
+          ];
+          AppleSelectedInputSources = [
+            {
+              InputSourceKind = "Keyboard Layout";
+              "KeyboardLayout ID" = 0;
+              "KeyboardLayout Name" = "U.S.";
+            }
+          ];
+        };
       };
     };
   };
